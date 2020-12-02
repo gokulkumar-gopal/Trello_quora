9d58b02ee140cc0c80dedf366dcbcc46a9704c42
Gokul Kumar Gopal <gokulkumar.gopal>
2020-12-02 23:07:33 +0530

    AdminController, CommonController, UserController functionality completed with all tests passed.

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AdminBusinessService.java
@@ -0,0 +1,28 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminBusinessService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private CommonBusinessService commonBusinessService;
+
+    @Autowired
+    private AdminBusinessService adminBusinessService;
+
+    public UserEntity userDelete(final String userUuid, final String authorization) throws AuthorizationFailedException, UserNotFoundException {
+        commonBusinessService.checkForAdminRole(authorization);
+        UserEntity userToDelete = commonBusinessService.getUserDetails(userUuid);
+        UserEntity deletedUser = userDao.deleteUser(userToDelete);
+        return deletedUser;
+    }
+}
---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/CommonBusinessService.java
@@ -0,0 +1,46 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CommonBusinessService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserEntity getUserDetails(final String userUuid) throws UserNotFoundException {
+        UserEntity userEntity = userDao.getUserByUuid(userUuid);
+        if(userEntity == null) {
+            throw new UserNotFoundException("USR-001", "User with entered uuid does not exist");
+        }
+        return userEntity;
+    }
+
+    @Transactional(propagation =  Propagation.REQUIRED)
+    public void authorizeUser(final String authorization) throws AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = userDao.getUserAuthToken(authorization);
+        if(userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+
+        if(userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get user details");
+        }
+    }
+
+    public void checkForAdminRole(final String authorization) throws AuthorizationFailedException {
+        if(!userDao.getUserAuthToken(authorization).getUser().getRole().equals("admin")) {
+            throw new AuthorizationFailedException("ATHR-003", "Unauthorized Access, Entered user is not an admin");
+        }
+    }
+
+}
